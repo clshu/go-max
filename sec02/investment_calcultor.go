e81b4db2384e69ab0c0d9c972c7a01a7ed6522df
clshu <clshu2020@gmail.com>
2025-04-12 15:59:45 +0800

    Sec02: 21. Type Conversions & Explicit Type Assignment

--- sec02/investment_calcultor.go
+++ sec02/investment_calcultor.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
+	var investmentAmount float64 = 1000
 	var expectedReturnRate = 5.5
-	var years = 10
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	var years float64 = 10
+	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
 	fmt.Println("Future Value of Investment: $", futureValue)
 
