8ef789e8787b876929b306e54fb0c86baa8ab1fa
clshu <clshu2020@gmail.com>
2025-04-12 17:36:23 +0800

    Sec02: 36. Exercise: Working with Functions

--- sec02_exercise01/profit_calculator.go
+++ sec02_exercise01/profit_calculator.go
@@ -11,11 +11,22 @@ func main() {
 	fmt.Scan(&expenses)
 	fmt.Println("Enter the tax rate (as a percentage):")
 	fmt.Scan(&taxRate)
-	taxRate = taxRate / 100 // Convert percentage to decimal
-	profit := revenue - expenses
-	tax := profit * taxRate
-	netProfit := profit - tax
+
+	profit, netProfit, ratio := calculateValues(revenue, expenses, taxRate/100)
 	fmt.Println("Earnings before tax:", profit)
 	fmt.Println("Earnings after tax:", netProfit)
-	fmt.Println("Ratio of Earnings before tax to Earnings after tax:", profit/netProfit)
+	fmt.Println("Ratio of Earnings before tax to Earnings after tax:", ratio)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func calculateValues(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	var profit = revenue - expenses
+	var tax = profit * taxRate
+	var netProfit = profit - tax
+	var ratio = profit / netProfit
+
+	return profit, netProfit, ratio
 }
