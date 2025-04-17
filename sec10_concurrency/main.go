4d1b107569ec5bb75f21a9a11ff72e6e6d51c89e
clshu <clshu2020@gmail.com>
2025-04-17 22:28:31 +0800

    Sec10: 152. Running Functions As Goroutines Sec10: 153. Understanding Goroutine Behavior Sec10: Introducing & Using Channels

--- sec10_concurrency/main.go
+++ sec10_concurrency/main.go
@@ -9,14 +9,18 @@ func greet(phrase string) {
 	fmt.Println("Hello!", phrase)
 }
 
-func slowGreet(phrase string) {
+func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
+	doneChan <- true // signal that the task is done
 }
 
 func main() {
-	greet("Nice to meet you!")
-	greet("How are you?")
-	slowGreet("How ... are ... you ...?")
-	greet("I hope you're liking the course!")
+	// go greet("Nice to meet you!")
+	// go greet("How are you?")
+	done := make(chan bool) // create a channel
+	go slowGreet("How ... are ... you ...?", done)
+	// go greet("I hope you're liking the course!")
+
+	<-done
 }
