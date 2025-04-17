acbf9b6943f7d916a80cae303d476e2c4b28d10d
clshu <clshu2020@gmail.com>
2025-04-17 17:07:45 +0800

    Sec08: 133. Splitting Slices Into Parameter

--- sec08_functions_deep_dives/main.go
+++ sec08_functions_deep_dives/main.go
@@ -1,8 +1,11 @@
 package main
 
 func main() {
+	numbers := []int{1, 2, 3, 4, 5, 100}
 	sum := sumup(100, 1, 2, 3, 4, 5, 100)
 	println("Sum of numbers:", sum)
+	anotherSum := sumup(100, numbers...)
+	println("Sum of another numbers:", anotherSum)
 }
 
 func sumup(startingValue int, numbers ...int) int {
