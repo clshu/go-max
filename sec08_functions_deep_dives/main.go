582bb1ad601c8af557f37f4b258f368428bce00e
clshu <clshu2020@gmail.com>
2025-04-17 17:04:29 +0800

    Sec08: 132. Using Variadic Functions

--- sec08_functions_deep_dives/main.go
+++ sec08_functions_deep_dives/main.go
@@ -1,63 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	fact := factorial(5)
-	factNonRecustion := factorialNonRecursion(5)
-	fmt.Println("Factorial of 5:", fact)
-	fmt.Println("Factorial of 5 (non-recursion):", factNonRecustion)
-
-	fabNumber := 10
-	fab := fibonacci(fabNumber)
-	fabNonRecursion := fibonacciNonRecursion(fabNumber)
-	fmt.Println("Fibonacci of 10:", fab)
-	fmt.Println("Fibonacci of 10 (non-recursion):", fabNonRecursion)
-
+	sum := sumup(100, 1, 2, 3, 4, 5, 100)
+	println("Sum of numbers:", sum)
 }
 
-// factorial of 5: 5! = 5 * 4 * 3 * 2 * 1 = 120
-func factorialNonRecursion(n int) int {
-	result := 1
-	for i := 1; i <= n; i++ {
-		result *= i
+func sumup(startingValue int, numbers ...int) int {
+	sum := startingValue
+	for _, number := range numbers {
+		sum += number
 	}
-	return result
-}
-
-func factorial(n int) int {
-	if n == 0 {
-		return 1
-	}
-	return n * factorial(n-1)
-}
-
-// fibonacci of 10:
-// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
-func fibonacci(n int) int {
-	if n <= 1 {
-		return n
-	}
-	return fibonacci(n-1) + fibonacci(n-2)
-}
-
-func fibonacciNonRecursion(n int) int {
-	if n < 0 {
-		panic("Negative number input")
-	}
-	fib := make([]int, n+1)
-	fib[0] = 0
-	if n >= 1 {
-		fib[1] = 1
-	}
-	if n >= 2 {
-		for i := 2; i <= n; i++ {
-			fib[i] = fib[i-1] + fib[i-2]
-		}
-	}
-	// The sequence is: fib
-	// Return the nth Fibonacci number
-	return fib[n]
+	return sum
 }
---
+++ sec08_functions_deep_dives/recursion/recursion.go
@@ -0,0 +1,64 @@
+package recursion
+
+import (
+	"fmt"
+)
+
+func recursion() {
+	fact := factorial(5)
+	factNonRecustion := factorialNonRecursion(5)
+	fmt.Println("Factorial of 5:", fact)
+	fmt.Println("Factorial of 5 (non-recursion):", factNonRecustion)
+
+	fabNumber := 10
+	fab := fibonacci(fabNumber)
+	fabNonRecursion := fibonacciNonRecursion(fabNumber)
+	fmt.Println("Fibonacci of 10:", fab)
+	fmt.Println("Fibonacci of 10 (non-recursion):", fabNonRecursion)
+
+}
+
+// factorial of 5: 5! = 5 * 4 * 3 * 2 * 1 = 120
+func factorialNonRecursion(n int) int {
+	result := 1
+	for i := 1; i <= n; i++ {
+		result *= i
+	}
+	return result
+}
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+// fibonacci of 10:
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+func fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+
+func fibonacciNonRecursion(n int) int {
+	if n < 0 {
+		panic("Negative number input")
+	}
+	fib := make([]int, n+1)
+	fib[0] = 0
+	if n >= 1 {
+		fib[1] = 1
+	}
+	if n >= 2 {
+		for i := 2; i <= n; i++ {
+			fib[i] = fib[i-1] + fib[i-2]
+		}
+	}
+	// The sequence is: fib
+	// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+	// Return the nth Fibonacci number
+	return fib[n]
+}
