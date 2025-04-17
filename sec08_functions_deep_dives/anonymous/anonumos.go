808c697fe96c6bb2190e8ec5e0b286ca321b2fb6
clshu <clshu2020@gmail.com>
2025-04-17 16:51:15 +0800

    Sec08: 131. Making Sense Of Recursion

---
+++ sec08_functions_deep_dives/anonymous/anonumos.go
@@ -0,0 +1,31 @@
+package anonymous
+
+import "fmt"
+
+func anonymousAndClosure() {
+	numbers := []int{1, 2, 3}
+
+	double := createTransformFunction(2)
+	triple := createTransformFunction(3)
+
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
+	fmt.Println(doubled)
+	fmt.Println(tripled)
+}
+
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
+	dNumbers := []int{}
+
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
+	}
+
+	return dNumbers
+}
+
+func createTransformFunction(factor int) func(int) int {
+	return func(n int) int {
+		return n * factor
+	}
+}
--- sec08_functions_deep_dives/main.go
+++ sec08_functions_deep_dives/main.go
@@ -1,31 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	numbers := []int{1, 2, 3}
+	fact := factorial(5)
+	factNonRecustion := factorialNonRecursion(5)
+	fmt.Println("Factorial of 5:", fact)
+	fmt.Println("Factorial of 5 (non-recursion):", factNonRecustion)
 
-	double := createTransformFunction(2)
-	triple := createTransformFunction(3)
+	fabNumber := 10
+	fab := fibonacci(fabNumber)
+	fabNonRecursion := fibonacciNonRecursion(fabNumber)
+	fmt.Println("Fibonacci of 10:", fab)
+	fmt.Println("Fibonacci of 10 (non-recursion):", fabNonRecursion)
 
-	doubled := transformNumbers(&numbers, double)
-	tripled := transformNumbers(&numbers, triple)
-	fmt.Println(doubled)
-	fmt.Println(tripled)
 }
 
-func transformNumbers(numbers *[]int, transform func(int) int) []int {
-	dNumbers := []int{}
-
-	for _, val := range *numbers {
-		dNumbers = append(dNumbers, transform(val))
+// factorial of 5: 5! = 5 * 4 * 3 * 2 * 1 = 120
+func factorialNonRecursion(n int) int {
+	result := 1
+	for i := 1; i <= n; i++ {
+		result *= i
 	}
-
-	return dNumbers
+	return result
 }
 
-func createTransformFunction(factor int) func(int) int {
-	return func(n int) int {
-		return n * factor
+func factorial(n int) int {
+	if n == 0 {
+		return 1
 	}
+	return n * factorial(n-1)
+}
+
+// fibonacci of 10:
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+func fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+
+func fibonacciNonRecursion(n int) int {
+	if n < 0 {
+		panic("Negative number input")
+	}
+	fib := make([]int, n+1)
+	fib[0] = 0
+	if n >= 1 {
+		fib[1] = 1
+	}
+	if n >= 2 {
+		for i := 2; i <= n; i++ {
+			fib[i] = fib[i-1] + fib[i-2]
+		}
+	}
+	// The sequence is: fib
+	// Return the nth Fibonacci number
+	return fib[n]
 }
